604c95cb19a8a2b127ca19602fb587ada8fbaa2a
FelipeManoel <felipe.miranda@alghorit.com.br>
2023-03-10 12:59:20 -0300

    correção

--- Alghoritmos e logica de programação com Java Script/Stage05Exer01/script.js
+++ Alghoritmos e logica de programação com Java Script/Stage05Exer01/script.js
@@ -1,22 +1,23 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-const numeroSorteado = Math.round(Math.random() * 10)
-let tentativas = 1
+let numeroSorteado = Math.round(Math.random() * 10)
 console.log(numeroSorteado)
+let tentativas = 1
 
 function apertarbotao(event) {
     
 
-    event.preventDefault();
-
+    
+    
     const inputNumber = document.querySelector("#inputNumber")
-
+    
     if(Number(inputNumber.value) == numeroSorteado){
+        console.log(inputNumber.value)
         screen1.classList.add("hide")
         screen2.classList.remove("hide")
 
         document.querySelector(".screen2 h2").innerText = 
-        `Parabéns você acertou em ${tentativas}` 
+        `Parabéns você acertou em ${tentativas} tentativa` 
     }
     
 
@@ -25,11 +26,13 @@ function apertarbotao(event) {
 }
 
 const btnTry = document.querySelector("#btnTry")
-const btnReset = documento.querySelector("#btnReset")
+const btnReset = document.querySelector("#btnReset")
 
 btnTry.addEventListener('click', apertarbotao)
 btnReset.addEventListener('click', function(){
     screen1.classList.remove("hide")
     screen2.classList.add("hide")
     tentativas = 1
+    numeroSorteado = Math.round(Math.random() * 10)
+    console.log(numeroSorteado)
 })

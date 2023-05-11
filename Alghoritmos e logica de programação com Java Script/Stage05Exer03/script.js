fc56a3d98f1f9343d57569380fa3b0582c50706a
FelipeManoel <felipe.miranda@alghorit.com.br>
2023-05-11 13:45:35 -0300

    ajustes nos botões

--- Alghoritmos e logica de programação com Java Script/Stage05Exer03/script.js
+++ Alghoritmos e logica de programação com Java Script/Stage05Exer03/script.js
@@ -1,5 +1,61 @@
-let play = document.querySelector('.play')
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+
+
+buttonPlay.addEventListener('click', function() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+})
+
+buttonPause.addEventListener('click', function() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonStop.classList.add('hide')
+    buttonSet.classList.remove('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+    buttonSoundOff.classList.add('hide')
+    buttonSoundOn.classList.remove('hide')
+})
+
+
+
+/*let play = document.querySelector('.play')
 let pause = document.querySelector('.pause')
 
-play.classList.add('hide')
-pause.classList.remove('hide')
+play.addEventListener('click', function iniciou)
+pause.addEventListener('click', function pausou)
+
+function iniciou() {
+    play.classList.add('hide');
+    pause.classList.remove('hide');
+}
+
+function pausou() {
+    pause.classList.add('hide');
+    play.classList.remove('hide');
+}
+*/
+
+
+
+
+
+
